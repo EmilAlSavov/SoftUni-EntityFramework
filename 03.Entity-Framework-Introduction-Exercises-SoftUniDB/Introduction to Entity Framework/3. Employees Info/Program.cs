6c1579ff1f8509807523b3f071c6ce837a25d4ba
Емил Савов <embakks@gmail.com>
2023-02-24 12:21:32 +0200

    exercises

---
+++ 03.Entity-Framework-Introduction-Exercises-SoftUniDB/Introduction to Entity Framework/3. Employees Info/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.EntityFrameworkCore;
+using SoftUni.Data;
+using SoftUni.Models;
+
+SoftUniContext context = new SoftUniContext();
+
+using (context)
+{
+    List<Employee> employees = await context.Employees.OrderBy(e => e.EmployeeId).ToListAsync();
+
+    foreach (var employee in employees)
+    {
+        Console.WriteLine($"{employee.FirstName} {employee.LastName} {employee.MiddleName} {employee.JobTitle} {employee.Salary:f2}");
+    }
+}
---
+++ 03.Entity-Framework-Introduction-Exercises-SoftUniDB/Introduction to Entity Framework/SoftUni/StartUp.cs
@@ -0,0 +1,34 @@
+﻿using SoftUni.Data;
+using SoftUni.Models;
+using System.Text;
+
+namespace SoftUni
+{
+
+    public class StartUp
+    {
+
+        static void Main(string[] args)
+        {
+            SoftUniContext softUniContext = new SoftUniContext();
+
+            string result = GetEmployeesFullInformation(softUniContext);
+
+            Console.WriteLine(result);
+        }
+
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            List<Employee> employees = context.Employees.OrderBy(e => e.EmployeeId).ToList();
+
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee.FirstName} {employee.LastName} {employee.MiddleName} {employee.JobTitle} {employee.Salary:f2}");
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
