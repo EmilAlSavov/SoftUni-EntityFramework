74dd04e8a1ffc97cd368b46b126b549b6c9b9297
EmilAlSavov <embakks@gmail.com>
2023-02-24 12:40:02 +0200

    more exercises

--- 03.Entity-Framework-Introduction-Exercises-SoftUniDB/Introduction to Entity Framework/SoftUni/StartUp.cs
+++ 03.Entity-Framework-Introduction-Exercises-SoftUniDB/Introduction to Entity Framework/SoftUni/StartUp.cs
@@ -12,11 +12,12 @@ namespace SoftUni
         {
             SoftUniContext softUniContext = new SoftUniContext();
 
-            string result = GetEmployeesFullInformation(softUniContext);
+            string result = GetEmployeesWithSalaryOver50000(softUniContext);
 
             Console.WriteLine(result);
         }
 
+        //Ex. 3
         public static string GetEmployeesFullInformation(SoftUniContext context)
         {
             StringBuilder sb = new StringBuilder();
@@ -29,6 +30,26 @@ namespace SoftUni
             }
 
             return sb.ToString().Trim();
+        }
+
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+        {
+            StringBuilder result = new StringBuilder();
+
+            var employees = context.Employees.Where(e => e.Salary > 50000)
+                .OrderBy(e => e.FirstName)
+                .Select(e => new
+            {
+                Firstname = e.FirstName,
+                Salary = e.Salary
+            }).ToList();
+
+            foreach (var employee in employees)
+            {
+                result.AppendLine($"{employee.Firstname} - {employee.Salary:f2}");
+            }
+
+            return result.ToString().TrimEnd();
         }
     }
 }
